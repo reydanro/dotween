945e80105522d56bc442f2acc4722034c3bf753c
Andrei Stanescu <ag@mindtreatstudios.com>
2015-04-03 23:34:09 +0300

    added custom ease functions

--- UnityExamples.Unity5/Assets/Basics.cs
+++ UnityExamples.Unity5/Assets/Basics.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using DG.Tweening.Core.Easing;
 
 public class Basics : MonoBehaviour
 {
@@ -12,10 +13,12 @@ public class Basics : MonoBehaviour
 		yield return new WaitForSeconds(1);
 
 		// Let's move the red cube TO 0,4,0 in 2 seconds
-		redCube.DOMove(new Vector3(0,4,0), 2);
+		EaseFunction innerEase = EaseFactory.StandardEaseFunction(Ease.Linear);
+		EaseFunction easeFunc = EaseFactory.StopMotion(3, innerEase);
+		redCube.DOMove (new Vector3 (0, 4, 0), 2).SetEase(easeFunc);
 
 		// Let's move the green cube FROM 0,4,0 in 2 seconds
-		greenCube.DOMove(new Vector3(0,4,0), 2).From();
+		greenCube.DOMove(new Vector3(0,4,0), 2).From().SetEase(EaseFactory.StopMotion(5));
 		
 		// Let's move the blue cube BY 0,4,0 in 2 seconds
 		blueCube.DOMove(new Vector3(0,4,0), 2).SetRelative();
@@ -25,6 +28,6 @@ public class Basics : MonoBehaviour
 		// To change its color, we'll have to use its material as a target (instead than its transform).
 		purpleCube.DOMove(new Vector3(6,0,0), 2).SetRelative();
 		// Also, let's set the color tween to loop infinitely forward and backwards
-		purpleCube.GetComponent<Renderer>().material.DOColor(Color.yellow, 2).SetLoops(-1, LoopType.Yoyo);
+		purpleCube.GetComponent<Renderer>().material.DOColor(Color.yellow, 2).SetLoops(-1, LoopType.Yoyo).SetEase(EaseFactory.StopMotion(4));
 	}
 }
---
+++ UnityExamples.Unity5/Assets/EaseFactory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using DG.Tweening.Core.Easing;
+
+public class EaseFactory {
+
+	public static EaseFunction StopMotion(int motionFps, EaseFunction innerEase = null) {
+		// Fallback
+		if (innerEase == null)
+			innerEase = StandardEaseFunction(DOTween.defaultEaseType);
+
+		// Compute the time interval in which we must re-evaluate the value
+		float motionDelay = 1.0f / motionFps;
+
+		return delegate(float time, float duration, float overshootOrAmplitude, float period) {
+			// Adjust the time so it's in steps
+			float steptime = time - (time % motionDelay);
+
+			// Evaluate the ease value based on the new step time
+			return innerEase(steptime, duration, overshootOrAmplitude, period);
+		};
+	}
+
+	// The easeType must not be INTERNAL_Custom
+	public static EaseFunction StandardEaseFunction(Ease easeType)
+	{
+		return delegate(float time, float duration, float overshootOrAmplitude, float period) {
+			return EaseManager.Evaluate(easeType, null, time, duration, overshootOrAmplitude, period);
+		};
+	}
+
+}
